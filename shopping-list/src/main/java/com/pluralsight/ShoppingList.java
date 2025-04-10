d9f25a56f1e0fb84e9a81467c19445196804088f
Eric Schwartz <erics273@gmail.com>
2025-04-10 14:32:01 -0400

    completed ShoppingList app

---
+++ shopping-list/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+public class ShoppingList {
+
+    public static void main(String[] args) {
+        System.out.println("California Pizza Kitchen BBQ chicken pizza");
+        System.out.println("French Fries");
+        System.out.println("Pepsi");
+        System.out.println("Coke");
+        System.out.println("Doritos");
+        System.out.println("Fruit Roll-ups");
+        System.out.println("California Pizza Pepperoni pizza");
+        System.out.println("Jamaican beef patty");
+        System.out.println("Frozen Burritos");
+        System.out.println("Fruit plate");
+
+    }
+
+}
