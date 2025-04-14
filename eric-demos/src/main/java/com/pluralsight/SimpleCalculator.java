4e1089bc0cef50d53586400d2952601a620d8c5c
Eric Schwartz <erics273@gmail.com>
2025-04-14 12:51:29 -0400

    simple calculator

--- eric-demos/src/main/java/com/pluralsight/SimpleCalculator.java
+++ eric-demos/src/main/java/com/pluralsight/SimpleCalculator.java
@@ -1,4 +1,38 @@
 package com.pluralsight;
 
+
+import java.util.Scanner;
+
 public class SimpleCalculator {
+
+    public static void main(String[] args) {
+
+        //fire up the scannner to allow me to collect input from the user
+        Scanner myScanner = new Scanner(System.in);
+
+        //let's greet our user
+        System.out.println("Welcome to my addition machine!");
+
+        //ask the user for the first number
+        System.out.println("What is the first number we are going to add? ");
+        int num1 = myScanner.nextInt();
+
+
+        //ask the user for the second number
+        System.out.println("What is the second number we are going to add? ");
+        int num2 = myScanner.nextInt();
+
+        //do the math
+        int sum = addNumbers(num1, num2);
+
+        //display the sum to the user
+        System.out.println("The sum is: " + sum);
+
+
+    }
+
+    public static int addNumbers(int num1, int num2){
+        return num1 + num2;
+    }
+
 }
